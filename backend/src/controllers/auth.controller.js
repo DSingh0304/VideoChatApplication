e732cce111edbc5d97db7670e71e15ac10e4a0e5
Deep Shekhar Singh <deepshekhar0306@gmail.com>
2025-08-29 00:29:23 +0530

    Project till now

--- backend/src/controllers/auth.controller.js
+++ backend/src/controllers/auth.controller.js
@@ -1,3 +1,4 @@
+import { createStreamUser } from "../lib/stream.js"
 import User from "../models/User.js"
 import jwt from "jsonwebtoken";
 export async function signup(req, res) {
@@ -16,12 +17,12 @@ export async function signup(req, res) {
             return res.status(400).json({ message: "Invalid email format" });
         }
 
-        const existingUser = await User.findOne({email});
-        if(existingUser){
-            return res.status(400).json({message: "User already exists :) Try different email."});
+        const existingUser = await User.findOne({ email });
+        if (existingUser) {
+            return res.status(400).json({ message: "User already exists :) Try different email." });
         }
 
-        const idx = Math.floor(Math.random()*100) + 1; // generate a number between 1-100
+        const idx = Math.floor(Math.random() * 100) + 1; // generate a number between 1-100
         const randomAvatar = `https://avatar.iran.liara.run/public/${idx}.png`
 
         const newUser = await User.create({
@@ -31,31 +32,71 @@ export async function signup(req, res) {
             profilePic: randomAvatar,
         })
 
-        // TODO: Create user in stearm as well
+        try {
+            await createStreamUser({
+                id: newUser._id.toString(),
+                name: newUser.fullName,
+                image: newUser.profilePic || "",
+            });
+            console.log(`Stream user created for ${newUser.fullName}`);
+        } catch (error) {
+            console.log(`Error creating a stream user: `, error);
+        }
 
-        const token = jwt.sign({userID:newUser._id},process.env.JWT_SECRET_KEY,{
-            expiresIn:"7d",
+        const token = jwt.sign({ userID: newUser._id }, process.env.JWT_SECRET_KEY, {
+            expiresIn: "7d",
         })
 
-        res.cookie('jwt',token,{
+        res.cookie('jwt', token, {
             maxAge: 7 * 24 * 60 * 60 * 1000,
             httpOnly: true, // Prevents XCSS attack
             sameSite: "strict", // Prevents CSRF attack
             secure: process.env.NODE_ENV === "production",
-        })
+        });
 
-        res.status(201).json({success:true , user:newUser });
+        res.status(201).json({ success: true, user: newUser });
 
     } catch (error) {
-        console.log("Error in signup controller" , error);
-        return res.status(500).json({message: "Internal Server Error"});
+        console.log("Error in signup controller", error);
+        return res.status(500).json({ message: "Internal Server Error" });
     }
 
 }
-export async function login(req, res){
-    res.send("Login Route");
+export async function login(req, res) {
+    try {
+        const { email, password } = req.body;
+        if (!email || !password) {
+            return res.status(400).json({ message: "All fields are required" });
+        }
+        const user = await User.findOne({ email });
+        if (!user) return res.status(401).json({ message: "invalid email or password" });
+
+        const isPasswordCorrect = await user.matchPassword(password);
+        if (!isPasswordCorrect) return res.status(401).json({ message: "invalid email or password" });
+
+        const token = jwt.sign({ userID: user._id }, process.env.JWT_SECRET_KEY, {
+            expiresIn: "7d",
+        })
+
+        res.cookie('jwt', token, {
+            maxAge: 7 * 24 * 60 * 60 * 1000,
+            httpOnly: true, // Prevents XCSS attack
+            sameSite: "strict", // Prevents CSRF attack
+            secure: process.env.NODE_ENV === "production",
+        });
+
+        res.status(200).json({ success: true, user });
+
+    } catch (error) {
+        console.log("Enter in login controller", error.message);
+        res.status(500).json({ message: "Internal Server Error" });
+    }
 
 }
 export function logout(req, res) {
-    res.send("Logout Route");
+    res.clearCookie("jwt")
+    res.status(200).json({ message: "Logout Successfull." });
+}
+export function onboard(res,req){
+    
 }
